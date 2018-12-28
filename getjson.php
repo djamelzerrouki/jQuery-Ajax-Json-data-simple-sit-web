64e1de77ab2678dfed0008dd04f56b76070ea4e7
zerrouki djamel <djameljimmizerrouki@gmail.com>
2018-12-28 01:14:31 +0100

    get json with php

--- getjson.php
+++ getjson.php
@@ -1,13 +1,27 @@
 <?php
-    $result_json = array('name' => 'test', 'age' => '16');
+$dbhost='localhost';
+$dbuser='root';
+$dbpass='root';
+$dbbase='departemant';
+$base= mysqli_connect($dbhost,  $dbuser, $dbpass, $dbbase);
 
-// headers for not caching the results
-header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+if (mysqli_connect_errno())
+  die('Could not connect: ' . mysql_error());
 
-// headers to tell that result is JSON
-header('Content-type: application/json');
+$return_arr = array();
+$sql="SELECT * FROM modules";
+if ($result = mysqli_query( $base, $sql )){
+    while ($row = mysqli_fetch_assoc($result)) {
+    $row_array['id_mod'] = $row['id_mod'];
+    $row_array['nom_mod'] = $row['nom_mod'];
+    $row_array['description'] = $row['description'];
+    $row_array['id_promo'] = $row['id_promo'];
 
-// send the result now
-echo json_encode($result_json);
+    array_push($return_arr,$row_array);
+   }
+ }
+
+mysqli_close($base);
+
+echo json_encode($return_arr);
 ?>
